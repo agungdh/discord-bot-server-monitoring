5d01c84bc39ac3e29ce882fc5b10d446b54e8fb3
AgungDH <agungdh@live.com>
2025-09-14 20:42:13 +0700

    init

---
+++ src/main/java/id/my/agungdh/discordbotservermonitoring/DiscordBotServerMonitoringApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.discordbotservermonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotServerMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordBotServerMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/discordbotservermonitoring/DiscordBotServerMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.discordbotservermonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotServerMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
